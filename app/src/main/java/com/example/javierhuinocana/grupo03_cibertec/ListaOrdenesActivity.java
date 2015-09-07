1d86c7daa160f44e94e24238b1ca346ae0c9105a
Javier Huiñocana <javier2315@hotmail.com>
2015-09-07 18:40:07 -0500

    Agregando Login

---
+++ app/src/main/java/com/example/javierhuinocana/grupo03_cibertec/ListaOrdenesActivity.java
@@ -0,0 +1,15 @@
+package com.example.javierhuinocana.grupo03_cibertec;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Javier Huiñocana on 07/09/2015.
+ */
+public class ListaOrdenesActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.lista_ordenes);
+    }
+}
