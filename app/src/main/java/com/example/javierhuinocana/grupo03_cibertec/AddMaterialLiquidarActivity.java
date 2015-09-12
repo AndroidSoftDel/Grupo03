b8520d3947de3ae31cac6ed6a8ae830a25b61aa2
JMartinez <josuemartinez@hotmail.com>
2015-09-12 09:24:35 -0500

    Actualizando Liquidar

--- app/src/main/java/com/example/javierhuinocana/grupo03_cibertec/AddMaterialLiquidarActivity.java
+++ app/src/main/java/com/example/javierhuinocana/grupo03_cibertec/AddMaterialLiquidarActivity.java
@@ -2,13 +2,21 @@ package com.example.javierhuinocana.grupo03_cibertec;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.Button;
+
+import com.example.javierhuinocana.grupo03_cibertec.adap_recyclerview.RVListadoAdapter;
+import com.example.javierhuinocana.grupo03_cibertec.adap_recyclerview.RVStockMaterialAdapter;
+import com.example.javierhuinocana.grupo03_cibertec.entities.StockMaterial;
 
 /**
  * Created by Javier Huiñocana on 11/09/2015.
  */
-public class AddMaterialLiquidarActivity extends AppCompatActivity {
+public class AddMaterialLiquidarActivity extends AppCompatActivity implements RVStockMaterialAdapter.RVStockMaterialAdapterCallBack{
+    private RecyclerView rvMaterial;
+    private RVStockMaterialAdapter rvStockMaterialAdapter;
 
     Button btnAgregar;
     @Override
@@ -19,6 +27,13 @@ public class AddMaterialLiquidarActivity extends AppCompatActivity {
         btnAgregar=(Button)findViewById(R.id.btnAgregarMaterial_AddMaterialLiquidar);
 
         btnAgregar.setOnClickListener(btnAgregarOnClickListener);
+
+        rvMaterial = (RecyclerView) findViewById(R.id.rvAddMaterial);
+        rvMaterial.setHasFixedSize(true);
+        rvMaterial.setLayoutManager(new LinearLayoutManager(AddMaterialLiquidarActivity.this));
+
+        rvStockMaterialAdapter = new RVStockMaterialAdapter(AddMaterialLiquidarActivity.this);
+        rvMaterial.setAdapter(rvStockMaterialAdapter);
     }
 
     View.OnClickListener btnAgregarOnClickListener = new View.OnClickListener() {
@@ -27,4 +42,9 @@ public class AddMaterialLiquidarActivity extends AppCompatActivity {
             finish();
         }
     };
+
+    @Override
+    public void onStockClick(StockMaterial stockMaterial, int position) {
+
+    }
 }
